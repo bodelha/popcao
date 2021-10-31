8d2159c6feca4f29d613da83e76e4227a8c8ae56
bodelha <bodelha@gmail.com>
2021-10-31 20:43:59 -0300

    locally integrated

--- api/popcao/settings.py
+++ api/popcao/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'popcao.wsgi.application'
 CORS_ORIGIN_ALLOW_ALL = False
 # CORS_ORIGIN_ALLOW = True
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:5500',
+    'http://127.0.0.1:5500',
     'http://localhost:8000',
     'http://localhost:8081',
 )
