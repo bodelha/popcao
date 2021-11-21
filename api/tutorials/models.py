6cd42e03a4655d4b14ff55827bed6d9f3ccabe74
bodelha <bodelha@gmail.com>
2021-11-21 01:53:54 -0300

    modeling

--- api/tutorials/models.py
+++ api/tutorials/models.py
@@ -1,27 +1,59 @@
 from django.db import models
+from datetime import timedelta
+from django.db.models.deletion import CASCADE
+
+from django.db.models.fields.related import ForeignKey
+# from api.tutorials.views import services
 
 
-# class Tutorial(models.Model):
-#     title = models.CharField(max_length=70, blank=False, default='')
-#     description = models.CharField(max_length=200,blank=False, default='')
-#     published = models.BooleanField(default=False)
-
+class Breed(models.Model):
+    id_breed = models.BigAutoField(verbose_name='Id', primary_key=True, db_column='Id')
+    name_breed = models.CharField(verbose_name='Raça', max_length=50, db_column='Raca')
+    groomable = models.BooleanField(verbose_name='Tosa Estética', default=False, db_column='TosaEstetica')
+    last_update = models.DateTimeField(auto_now=True, db_column='LastUpdate')
 
 class Service(models.Model):
-    id_service = models.BigAutoField(verbose_name='ServiceId', primary_key=True)
-    breed = models.IntegerField(blank=False, default=42)
-    name_service = models.CharField(verbose_name='nameService', max_length=70, blank=False, default="0:30:00")
-    small_time = models.DurationField(verbose_name='smallTime', blank=False, default="0:30:00")
-    medium_time = models.DurationField(verbose_name='mediumTime', blank=False, default="0:35:00")
-    large_time = models.DurationField(verbose_name='largeTime', blank=False, default="0:40:00")
+    id_service = models.BigAutoField(primary_key=True, db_column='Id')
+    breeds_set = models.ManyToManyField(to=Breed, through='Group')
+    name_service = models.CharField(max_length=80, verbose_name='Nome Serviço', db_column='NomeServico')
+    time_service = models.DurationField(blank=True, null=True, db_column='TempoMedioGrupoRaca')
+    short_fur = models.DurationField(blank=True, null=True, db_column='TempoMedioPeloCurto')
+    medium_fur = models.DurationField(blank=True, null=True, db_column='TempoMedioPeloMedio')
+    long_fur = models.DurationField(blank=True, null=True, db_column='TempoMedioPeloLongo')
+    last_update = models.DateTimeField(auto_now=True, db_column='LastUpdate')
+
+class Group(models.Model):
+    breed = models.ForeignKey(to=Breed, on_delete=models.CASCADE)
+    id_service = models.ForeignKey(to=Service, on_delete=CASCADE)
 
 class Tutor(models.Model):
-    cellphone_tutor = models.IntegerField(primary_key=True)
+    id_tutor = models.BigAutoField(primary_key=True, db_column='Id', default=42)
     name_tutor = models.CharField(max_length=50, blank=False)
-    endereco_tutor = models.CharField(max_length=150, blank=True)
-    # pet_tutor = models.ForeignObject(to=Pet)
+    cellphone1 = models.IntegerField(verbose_name="Telefone Principal", db_column='Telefone1', default=-1)
+    cellphone2 = models.IntegerField(verbose_name="Outro Telefone", null=True, db_column='Telefone2', blank=True)
+    endereco_tutor = models.CharField(max_length=300, blank=True)
+    last_update = models.DateTimeField(auto_now=True, db_column='LastUpdate')
 
 class Pet(models.Model):
-    id_pet = models.AutoField(verbose_name='PetId', primary_key=True)
-    name_pet = models.CharField(max_length=50, blank=False, default='n')
-    tutor_pet = models.ForeignKey(to=Tutor, on_delete=models.CASCADE)
+    SEX_CHOICES = (
+        ('M', 'Macho'),
+        ('F', 'Fêmea'),
+        ('D', 'Desconhecido'),
+    )
+    id_pet = models.AutoField(verbose_name='PetId', primary_key=True, db_column='Id')
+    name_pet = models.CharField(max_length=50, blank=False, default='n', db_column='Nome')
+    breed_id = models.ForeignKey(to=Breed, on_delete=models.CASCADE, db_column='RacaId', default=-1)
+    tutor_id = models.ForeignKey(to=Tutor, on_delete=models.CASCADE, db_column='TutorId', default= -1)
+    puppy = models.BooleanField(verbose_name='Filhote', default=False, db_column='Filhote')
+    sex = models.CharField(max_length=1, verbose_name='Sexo', db_column='Sexo', choices=SEX_CHOICES, default='D')
+    last_update = models.DateTimeField(auto_now=True, db_column='LastUpdate')
+
+class ServiceOrder(models.Model):
+    id_attendance = models.BigAutoField(primary_key=True, db_column='Id')
+    tutor_id = models.ForeignKey(to=Tutor, on_delete=models.CASCADE, db_column='TutorId')
+    pet_ids = models.ManyToManyField(to=Pet, through='Attendance')
+    date = models.DateTimeField(auto_now=True)
+
+class Attendance(models.Model):
+    service_order = models.ForeignKey(to=ServiceOrder, on_delete=models.CASCADE)
+    pet_id = models.ForeignKey(to=Pet, on_delete=models.CASCADE)
--- api/tutorials/urls.py
+++ api/tutorials/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
     # url(r'^api/tutorials/published$', views.tutorial_list_published),
     url(r'^api/services', views.services),
-    url(r'^api/service/(?P<pk>\d+)$', views.service)
+    url(r'^api/service/(?P<pk>\d+)$', views.service),
+    url(r'^settings', views.settings)
     # url(r'^api/service', views.service),
 ]
